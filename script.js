66cbc1feebb55c8c08ea07e19964da3ec0f79f15
igormlshkv <115819101+igormlshkv@users.noreply.github.com>
2022-11-10 11:31:19 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,31 @@ function goTop(){
     var myDiv = document.querySelector("article");
     myDiv.scrollTop = 0;
 }
+
+
+const article = document.querySelector(".article")
+var darkMode = localStorage.getItem("dark_mode");
+
+function DarkMode() {
+    article.classList.add("darkmode");
+  localStorage.setItem("dark_mode", "yes");
+};
+
+function LightMode() {
+  article.classList.remove("darkmode");
+  localStorage.setItem("dark_mode", "no");
+};
+
+if (darkMode === "yes") {
+  DarkMode(); 
+}
+function DarkModeToggle(){
+    darkMode = localStorage.getItem("dark_mode");
+    if (darkMode === "no") {
+        DarkMode();
+      } else {
+        LightMode();
+      }
+
+
+}
